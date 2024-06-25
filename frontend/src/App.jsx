e34b115cd89dd4f2ec519dd9a1423d6c7b391224
Liav <liavcohen955@gmail.com>
2024-06-25 10:35:09 +0300

    add basic navigation routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Login, Register, NotFound, Home } from './pages';
+import ProtectedRoute from "./components/ProtectedRoute"
+
+
+function logout() {
+  localStorage.clear()
+  return <Navigate to="/login"></Navigate>
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register></Register>
+}
 
 function App() {
 
   return (
     <>
- 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogout/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
