5a57b071083de7e95924a4c7e94ce2024889cb35
Liav <liavcohen955@gmail.com>
2024-06-25 10:59:55 +0300

    add code to NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 function NotFound() {
-    return <div>NotFound</div>
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>Oops! The page you're looking for does not exists</p>
+    </div>
 }
 
 export default NotFound
